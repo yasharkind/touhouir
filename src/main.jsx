9c018204232aa0da8385743dfff0844ad76f2680
yasharkind <yasharkind95@gmail.com>
2025-06-11 23:41:45 +0330

    +marisa

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import HomePage from './pages/HomePage.jsx'
 import { BrowserRouter, HashRouter, Route, Routes } from 'react-router'
--- src/pages/Characters/Marisa.jsx
+++ src/pages/Characters/Marisa.jsx
@@ -14,17 +14,21 @@ const Marisa = () => {
                 <img className="m-auto" src="/chars/marisa/marisa2.png" />
                 <img className="m-auto" src="/chars/marisa/marisa1.png" />
             </div>
-            <div className="content content-characters" dir="rtl">
+            <div className="content content-characters color-1" dir="rtl">
                 <div className="text-2xl text-right w-full mt-4 mb-4" >
                     معرفی:
                 </div>
-                <div className="text-xl text-right w-full">
-                    نام کامل: 霧雨魔理沙 Marisa Kirisame
-                    <br />
-                    نژاد: انسان
-                    <br />
-                    <br />
-                    ماریسا یک جادوگر معمولی، یکی از دو شخصیت اصلی بازی های توهو هست که از بازی دوم وارد توهو شد.  ماریسا شخصیتی پرانرژی، خودجوش و گاهی هم لجباز داره، و به جمع‌آوری کتاب‌ها و آیتم‌های جادویی (حتی اگر صاحبشون نباشه!) علاقه‌منده. حضور پررنگش تو بیشتر بازی‌ها و رابطه‌ی بامزه‌اش با شخصیت‌هایی مثل ریمو، باعث شده یکی از محبوب‌ترین شخصیت‌های دنیا‌ی توهو باشه.
+                <div className="flex flex-row">
+                    <div className="text-xl text-right w-full bg-touhou">
+                        نام کامل: 霧雨魔理沙 Marisa Kirisame
+                        <br />
+                        نژاد: انسان
+                        <br />
+                        <br />
+                        ماریسا یک جادوگر معمولی، یکی از دو شخصیت اصلی بازی های توهو هست که از بازی دوم وارد توهو شد.  ماریسا شخصیتی پرانرژی، خودجوش و گاهی هم لجباز داره، و به جمع‌آوری کتاب‌ها و آیتم‌های جادویی (حتی اگر صاحبشون نباشه!) علاقه‌منده. حضور پررنگش تو بیشتر بازی‌ها و رابطه‌ی بامزه‌اش با شخصیت‌هایی مثل ریمو، باعث شده یکی از محبوب‌ترین شخصیت‌های دنیا‌ی توهو باشه.
+                    </div>
+                    <div className="left-img w-1/2 h-96 bg-[url(/chars/marisa/left.jpg)] h-96 object-cover bg-position-[25%] mask-r-from-30% mask-r-to-90% rounded-[12px] ">
+                    </div>
                 </div>
                 <div className="text-2xl text-right w-full mt-4 mb-4" >
                     آهنگ ها:
@@ -37,13 +41,14 @@ const Marisa = () => {
                         <source src="/chars/marisa/theme.ogg" type="audio/ogg" />
                     </audio>
                 </div>
+                {/* <iframe className="mb-[32px] mt-[16px]" width="560" height="315" src="https://www.youtube.com/embed/TFHPmeuPS8Y?si=jn2sf98aiuwbzubW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
                 <div dir="ltr" className="rounded-xl">
                     <ImageSlider className="rounded-xl" images={images} />
                 </div>
                 <a href="https://en.touhouwiki.net/wiki/Marisa_Kirisame" target="_blank" className="self-center">
-                    <button  className="m-4">
+                    <button className="m-4">
                         <div className="sm:text-sm md:text-xl text-sm ">
-                        بیشتر (touhou wiki)
+                            بیشتر (touhou wiki)
                         </div>
                     </button>
                 </a>
@@ -51,7 +56,7 @@ const Marisa = () => {
             <Footer />
 
         </div>
-    </div>
+    </div >
     </>
 }
 
