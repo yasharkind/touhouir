35685664b5fd1752dc2b10539b18a5a9b4a76eee
yasharkind <yasharkind95@gmail.com>
2025-06-14 21:18:13 +0330

    added sound

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -8,6 +8,7 @@ const ChatPage = () => {
     const ws = useRef(null)
     const [_name, setName] = useState("")
     const [_messages, setMessages] = useState([])
+    const audioRef = useRef(null);
     
     useState(() => {
         console.log("refreshed")
@@ -20,6 +21,7 @@ const ChatPage = () => {
         }
 
         ws.current.onmessage = (event) => {
+            audioRef.current.play()
             const message = event.data
             setMessages(l => l.concat(message))
         }
@@ -39,6 +41,11 @@ const ChatPage = () => {
         }
     }
     return <>
+    <audio
+        ref={audioRef}
+        src="/chatpage/send.ogg"
+        preload="auto"
+      />
         <div className='flex flex-col h-screen scheme-home'>
             <div className="w-full h-9/10 max-h-9/10 overflow-auto flex flex-col-reverse p-4" >
                 {_messages.toReversed().map(message => {
